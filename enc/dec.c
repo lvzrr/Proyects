5e0b4f1a3daad77f113b0767ab68d7308cc2ce69
lvzrr <luxoresl@gmail.com>
2024-12-09 20:02:28 +0100

    support for multiple files

--- enc/dec.c
+++ enc/dec.c
@@ -2,19 +2,21 @@
 int main(int argc, char *argv[]) {
   if (argc > 1) {
     for (int i = 1; i < argc; i++) {
-      switch (decrypt_file(argv[i])) {
-      case 0:
-        printf("[+] %s\n", argv[i]);
-        break;
-      case 1:
-        printf("[Error] %s\n", argv[i]);
-        return 1;
-      default:
-        printf("[?] %s\n", argv[i]);
-        return 1;
+      if (strstr(argv[i], ".crypt") == NULL) {
+        switch (decrypt_file(argv[i])) {
+        case 0:
+          printf("[+] %s\n", argv[i]);
+          break;
+        case 1:
+          printf("[Error] %s\n", argv[i]);
+          return 1;
+        default:
+          printf("[?] %s\n", argv[i]);
+          return 1;
+        }
       }
+      return 0;
     }
-    return 0;
   } else {
     printf("Usage: %s <filename>\n", argv[0]);
     return 1;
