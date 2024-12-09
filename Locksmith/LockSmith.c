f432f722dda319880fc4a392b001f10cf00583bf
lvzrr <luxoresl@gmail.com>
2024-12-09 20:45:09 +0100

    renamed proyects

--- Locksmith/LockSmith.c
+++ Locksmith/LockSmith.c
@@ -1,4 +1,4 @@
-#include "pass.h"
+#include "LockSmith.h"
 #include <stdio.h>
 int main(int argc, char **args) {
   printf("\nGenerated Password:\n%s\n\n", gen_pass());
--- pswd/run.sh
+++
@@ -1,3 +0,0 @@
-gcc pswd.c -o p -O3
-chmod +x p
-./p
